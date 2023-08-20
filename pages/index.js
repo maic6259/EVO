2b7fafdf6236752ff951e9f7592db523e2605051
maic6259 <131110376+maic6259@users.noreply.github.com>
2023-08-20 09:35:06 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,30 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import Header from '../components/Header'
-import Feed from '@/components/Feed'
-import UploadModal from '@/components/UploadModal'
+import Head from 'next/head';
+import Header from '../components/Header';
+import TopBar from '../components/TopBar'; // Importa el componente TopBar
+import Feed from '@/components/Feed';
+import UploadModal from '@/components/UploadModal';
+import Footer from '../components/Footer';
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <div className='bg-gray-50 min-h-screen'>
-      <Head>
-        <title>Agrolivery</title>
-        <meta name="description" content="E-commerce para conectar emprendedores y agricultores con clientes" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {/* Header */}
-      <Header />
-      {/* Feed */}
-      <Feed />
-      {/* Modal */}
-      <UploadModal />
+        <Head>
+          <title>Agrolivery</title>
+          {/* ... tus metadatos ... */}
+        </Head>
+        {/* Header */}
+        <Header />
+        {/* Feed */}
+        <Feed />
+        {/* Modal */}
+        <UploadModal />
+        {/* Componente TopBar */}
+        <TopBar />
+        {/* Componente Footer */}
+        <Footer />
       </div>
     </>
-  )
+  );
 }
